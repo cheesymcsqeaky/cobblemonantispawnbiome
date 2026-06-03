d698a86692b02c86fea7c592552c9680a861d23d
CheesyMcSqeaky <cheesymcsqeaky@gmail.com>
2026-06-02 22:43:43 -0600

    upload

---
+++ src/client/java/com/github/cheesymcsqeaky/cobblemonantispawnbiome/client/CobblemonAntispawnBiomeClient.java
@@ -0,0 +1,10 @@
+package com.github.cheesymcsqeaky.cobblemonantispawnbiome.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonAntispawnBiomeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
